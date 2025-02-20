a48e1436198704bb22c535c8b359916f50ddecca
IsraBA <israelbenari1000@gmail.com>
2025-02-20 16:09:12 +0200

    add '/generate-pdf-batch' route

--- functions/convertHtmlToPdf.js
+++ functions/convertHtmlToPdf.js
@@ -1,12 +1,21 @@
 // functions/convertHtmlToPdf.js
 const puppeteer = require('puppeteer');
+require('dotenv').config();
 
 // פונקציה להמרת HTML ל-PDF
 const convertHtmlToPdf = async (html) => {
-    const browser = await puppeteer.launch({
-        headless: true, // מתאים לסביבת שרת
-        args: ['--no-sandbox'],
-    });
+    const puppeteerObj = process.env.ENVIRONMENT === 'dev' ?
+        {
+            headless: true, // מתאים לסביבת שרת
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        } :
+        {
+            headless: true, // מתאים לסביבת שרת
+            executablePath: '/usr/bin/chromium-browser', // נתיב לדפדפן Chromium המותקן
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        };
+
+    const browser = await puppeteer.launch(puppeteerObj);
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0', timeout: 60000 });
 
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const cors = require('cors'); // ייבוא CORS
+const archiver = require('archiver'); // ספרייה ליצירת ZIP
 const { convertHtmlToPdf } = require('./functions/convertHtmlToPdf');
 
 const app = express();
@@ -39,6 +40,60 @@ app.post('/generate-pdf', async (req, res) => {
     } catch (error) {
         console.error('Error generating PDF:', error);
         res.status(500).send('Failed to generate PDF');
+    }
+});
+
+// להמרת מספר HTMLs, והחזרה כקובץ ZIP
+app.post('/generate-pdf-batch', async (req, res) => {
+    try {
+        const apiKey = req.headers['x-api-key'];
+        console.log(`Incoming batch request with API key: ${apiKey}`);
+
+        if (apiKey !== process.env.PDF_API_KEY) {
+            return res.status(403).send('Forbidden: Invalid API Key');
+        };
+
+        const { documents } = req.body;
+        if (!documents || !Array.isArray(documents) || documents.length === 0) {
+            return res.status(400).send('Missing or invalid "documents" array in request body');
+        };
+
+        // הגדרת הפרמטרים לקובץ ה-ZIP
+        res.setHeader('Content-Type', 'application/zip');
+        res.setHeader('Content-Disposition', 'attachment; filename="pdfs.zip"');
+
+        // יצירת ארכיון ZIP ו"חיבור" ל-Response
+        const archive = archiver('zip', { zlib: { level: 9 } });
+        archive.on('error', (err) => {
+            throw err;
+        });
+        archive.pipe(res);
+
+        // לעבור על כל קובץ וליצור PDF מ-HTML
+        for (const doc of documents) {
+            console.time(`PDF Conversion Time - ${doc.filename}`); // התחלת מדידת זמן
+
+            // 1) המרת ה-HTML ל-PDF
+            const pdfUint8Array = await convertHtmlToPdf(doc.html);
+
+            // המרה ל-Buffer
+            const pdfBuffer = Buffer.from(pdfUint8Array);
+            // console.log(`Generated PDF for ${doc.filename}:`, pdfBuffer);
+
+            // 2) הוספת ה-PDF לארכיון עם שם קובץ
+            const filename = doc.filename || `document-${Date.now()}.pdf`;
+            archive.append(pdfBuffer, { name: filename });
+
+            console.timeEnd(`PDF Conversion Time - ${doc.filename}`); // סיום מדידת זמן
+        }
+
+        // סיום הארכיון
+        await archive.finalize();
+        console.log('Sent batch ZIP of PDFs');
+
+    } catch (error) {
+        console.error('Error generating batch PDFs:', error);
+        res.status(500).send('Failed to generate batch PDFs');
     }
 });
 
